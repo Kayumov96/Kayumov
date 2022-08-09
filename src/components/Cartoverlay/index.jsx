f10086f69c684ed57bc67020e7d90394449997b1
ArabboyKayumov <arabboykayumov@gmail.com>
2022-08-09 22:53:17 +0300

    components created

---
+++ src/components/Cartoverlay/index.jsx
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import { Btn, CartItems, Colors, Select, Sizes } from "./style";
+import MainContext from "../Context";
+
+class CartOverlay extends Component {
+  static contextType = MainContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      // eur: "",
+      // usd: "",
+      // jpy: "",
+      // showCart: false,
+      // totalPrice: 0,
+      // valute: "$",
+      count: props.value.count || 1,
+      // cart: this?.props?.value || [],
+    };
+    // this.showMyCart = this.showMyCart.bind(this);
+  }
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps?.cart !== this?.context?.cart)
+  //     this.fetchData(this.context?.cart);
+  // }
+  // showMyCart() {
+  //   this.setState((state) => ({
+  //     showCart: !state.showCart,
+  //   }));
+  // }
+  getColor(type) {
+    // console.log(type, "rang");
+    switch (type) {
+      case "grey":
+        return <Colors grey />;
+        break;
+      case "pink":
+        return <Colors pink />;
+        break;
+      case "yellow":
+        return <Colors yellow />;
+        break;
+      case "green":
+        return <Colors green />;
+        break;
+      case "black":
+        return <Colors black />;
+        break;
+      case "red":
+        return <Colors red />;
+        break;
+      default:
+        <Colors white />;
+        break;
+    }
+  }
+
+  updateCount(num) {
+    for (let i in this.context.cart) {
+      if (this.props.value.id === this.context.cart[i].id) {
+        this.context.cart[i].count = this.context.cart[i].count ? this.context.cart[i].count+num : num+1;
+        this.setState({count: this.context.cart[i].count});
+        let total = 0;
+        let count = 0;
+        for (let j in this.context.cart) {
+          total+=parseFloat(this.context.cart[j].price * (this.context.cart[j].count || 1))
+          count+=parseInt(1 * (this.context.cart[j].count || 1))
+        }
+        document.getElementById("total-price").innerText = total;
+        document.getElementById("all-items-count").innerText = count;
+
+        break;
+      }
+    }
+    return this.context.cart;
+  }
+  render() {
+    const { value } = this.props;
+    // const context = this.context;
+    return (
+      <>
+        <CartItems key={value.id}>
+          <CartItems.Description>
+            <span> {value?.title}</span>
+            <h4>
+              {this.state.valute}
+              {""}
+              {value?.price}
+            </h4>
+            Size:
+            <div style={{ display: "flex", gap: "4px" }}>
+              {value?.size.map((val, index) => (
+                <Sizes key={index}>{val}</Sizes>
+              ))}
+            </div>
+            Color:
+            <div style={{ display: "flex", gap: "6px" }}>
+              {value?.color.map((v) => (
+                <Sizes>{this.getColor(v)}</Sizes>
+              ))}
+            </div>
+          </CartItems.Description>
+          <Select.BtnDiv
+            style={{
+              width: "8%",
+              height: "100%",
+              flexDirection: "column",
+              padding: "6px 3px",
+            }}
+          >
+            <Btn onClick={() => console.log(this.updateCount(1))}>
+              +
+            </Btn>
+            {this.state.count}
+            <Btn onClick={() => console.log(this.updateCount(-1))}>
+              -
+            </Btn>
+          </Select.BtnDiv>
+          <CartItems.Img
+            src={value.src}
+            alt="product"
+            // style={{ width: "25px" }}
+          />
+        </CartItems>
+      </>
+    );
+  }
+}
+
+export default CartOverlay;
---
+++ src/components/Cartoverlay/style.js
@@ -0,0 +1,120 @@
+import styled from "styled-components";
+import { ReactComponent as cart1 } from "../../assets/icons/cart1.svg";
+
+const Select = styled.select`
+  outline: none;
+  border: none;
+  margin-right: 8px;
+  cursor: pointer;
+`;
+Select.Button = styled.button`
+  box-sizing: ${({ green }) => (green ? "none" : "border-box")};
+  display: flex;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 32px;
+  position: static;
+  width: 140px;
+  font-size: 14px;
+  height: 43px;
+  left: 0px;
+  top: 0px;
+  background: ${({ green }) => (green ? "#5ece7b" : "#ffffff")};
+  border: ${({ green }) => (green ? "none" : "1px solid #1d1f22")};
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+  margin: 0px 10px;
+`;
+Select.BtnDiv = styled.div`
+  display: flex;
+  height: 100px;
+  padding: 3px;
+  width: inherit;
+  background: #ffffff;
+  width: inherit;
+  justify-content: space-between;
+  align-items: center;
+  position: sticky;
+  bottom: 0;
+`;
+
+const CartItems = styled.div`
+  margin: 8px 0;
+  width: 100%;
+  height: 170px;
+  display: flex;
+`;
+CartItems.Description = styled.div`
+  width: 44%;
+  height: 100%;
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+`;
+CartItems.Img = styled.img`
+  width: 44%;
+  height: 100%;
+`;
+CartItems.Amount = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #1d1f22;
+  color: #ffffff;
+  position: absolute;
+  top: -10px;
+  left: 9px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const handleColor = (type) => {
+  switch (type) {
+    case "grey":
+      return "#696969";
+      break;
+    case "pink":
+      return "#FFC0CB";
+      break;
+    case "yellow":
+      return "#FFFF00";
+      break;
+    case "green":
+      return "#008000";
+      break;
+    case "black":
+      return "#000000";
+      break;
+    case "red":
+      return "#FF0000";
+      break;
+    default:
+      return "#ffffff";
+      break;
+  }
+};
+const Sizes = styled.div`
+  width: 26px;
+  height: 26px;
+  display: flex;
+  border: 1px solid #000000;
+  align-items: center;
+  justify-content: center;
+  /* :active {
+    
+    color: #ffffff;
+  } */
+`;
+const Btn = styled.button`
+  width: 20px;
+  height: 20px;
+`;
+const Colors = styled.div`
+  width: 26px;
+  background-color: ${({ type }) => handleColor(type) || "#696969"};
+  height: 26px;
+`;
+export { Btn, Sizes, Select, CartItems, Colors };
